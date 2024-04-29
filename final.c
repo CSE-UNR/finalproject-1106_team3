e080cc1bdcbc45a4818aae4cbd965615198d8d7c
BruhJalen <jbanks@Unbuntu22.04.myguest.virtualbox.org>
2024-04-29 15:08:56 -0700

    start

---
+++ final.c
@@ -0,0 +1,34 @@
+//Alysia Carr and Jalen Banks
+//4/29/202
+
+#include <stdio.h>
+
+int main(){
+
+
+}
+
+
+void editMenuChoice(){
+
+}
+
+void getMenuChoice(){
+
+}
+
+void saveImage(){
+
+}
+
+void editFunction(){
+
+}
+
+void dimBrighten(){
+
+}
+
+void crop(){
+
+}
