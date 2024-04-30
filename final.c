0e9b02f8ac4f7c7a19102410bc6f83c892939856
BruhJalen <jbanks@Unbuntu22.04.myguest.virtualbox.org>
2024-04-30 11:25:07 -0700

    work 1

--- final.c
+++ final.c
@@ -2,9 +2,26 @@
 //4/29/202
 
 #include <stdio.h>
+#define maxY 50
+#define maxX 50
+//Prototypes
+void readFile(FILE* fp, int userChoice);
+
+
+
 
 int main(){
+	FILE* fp;
+	int userChoice;
+	int userEdit; //For edit menu
+	
+	printf("Enter your choice: \n");
+	scanf("%d", userChoice);
+	readFile(fp, userChoice);
 
+}
+
+void getMenuChoice(){
 
 }
 
@@ -13,22 +30,39 @@ void editMenuChoice(){
 
 }
 
-void getMenuChoice(){
-
-}
 
 void saveImage(){
 
 }
 
+
 void editFunction(){
 
 }
+
 
 void dimBrighten(){
 
 }
 
+
 void crop(){
 
+}
+
+
+//Current Work
+void readFile(FILE* fp,int userChoice){
+	if(userChoice == 1){
+	fp = fopen("test_image.txt", "r");
+		if(fp == NULL){
+			printf("Image could not be opened \n");
+		} 
+		for(int i = 0; i < maxX; i++){
+			for(int j = 0; j < maxY; j++){
+			
+			}
+		{
+	} 
+	fclose(fp);
 }
